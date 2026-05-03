448154a75569036163e2fc782c0c276ed5e9f8f7
Santiago Ferrero <santifnob@gmail.com>
2026-05-03 12:32:56 -0300

    Fix Filtrar Por Button

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const queryClient = new QueryClient()
 
